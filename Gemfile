dc9994cb14538a164b27196a24cddcfc7b47f585
vitaly omelchuk <vitaly.omelchuk@gmail.com>
2014-05-14 13:08:14 +0300

    user model

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby '2.1.1'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.1'
 gem 'bootstrap-sass', '2.3.2.0'
+gem 'bcrypt-ruby', '3.1.2'
 
 group :development, :test do
   gem 'sqlite3'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 6 }
+
+  before_save { self.email = email.downcase }
+
   has_many :microposts
+  has_secure_password
 end
